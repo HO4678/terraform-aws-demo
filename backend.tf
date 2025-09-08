ffef244f6203f4bb73028486a921eb926b2112a0
HO4678 <123117953+HO4678@users.noreply.github.com>
2025-09-08 12:08:06 -0400

    Delete backend.tf

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "tf-bckend3"
-    key            = "terraform-state2/terraform.tfstate"
-    region         = "us-east-1"   # Must match the actual bucket region
-    encrypt        = true
-  }
-}
