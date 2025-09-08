d583b1c5f4f8715dfba2c78156b7b1271392dba6
HO4678 <123117953+HO4678@users.noreply.github.com>
2025-09-08 12:00:40 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "tf-bckend3"
-    key    = "terraform-state2"
-    region = "us-east-1"
+    bucket         = "tf-bckend3"
+    key            = "terraform-state2/terraform.tfstate"
+    region         = "us-east-1"   # Must match the actual bucket region
+    encrypt        = true
   }
 }
